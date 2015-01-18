5a0e0c56a368c13bfb58b32d2751e4e31ef1a7c9
Viral Savani <viralsavani@gmail.com>
2015-01-18 14:13:37 -0800

    Document representing inserting through Java driver

---
+++ src/main/java/com/tengen/InsertTest.java
@@ -0,0 +1,29 @@
+package com.tengen;
+
+import com.mongodb.*;
+import org.bson.types.ObjectId;
+
+import java.net.UnknownHostException;
+import java.util.Arrays;
+
+/**
+ * Created by VIRAL on 1/18/2015.
+ */
+public class InsertTest {
+    public static void main(String[] args) throws UnknownHostException {
+        MongoClient client = new MongoClient();
+        DB courseDB = client.getDB("course");
+        DBCollection collection = courseDB.getCollection("insertTest");
+
+        collection.drop();
+
+        DBObject dbObject = new BasicDBObject("Name", "Viral").append("Method", "From Java Drive");
+        DBObject dbObject2 = new BasicDBObject("_id", new ObjectId()).append("Name", "Anand").append("Method", "AsList");
+
+        //dbObject.removeField("_id"); //Deletes the entire document
+
+        System.out.println(dbObject);
+        collection.insert(Arrays.asList(dbObject,dbObject2));
+        System.out.println(dbObject);
+    }
+}
