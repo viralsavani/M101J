2152cb0525dd376c9a083d48eabd5eed8dec77d5
Viral Savani <viralsavani@gmail.com>
2015-02-20 21:41:19 -0800

    Final Exam Task.

--- src/main/java/com/tengen/App.java
+++ src/main/java/com/tengen/App.java
@@ -1,13 +1,32 @@
 package com.tengen;
 
+import com.mongodb.*;
+
+import java.io.IOException;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main(String[] args) throws IOException {
+        MongoClient c =  new MongoClient(new MongoClientURI("mongodb://localhost"));
+        DB db = c.getDB("finaltask");
+        int i =0;
+        DBCollection album = db.getCollection("albums");
+        DBCollection image = db.getCollection("images");
+
+        DBCursor cur = image.find();
+        cur.next();
+
+        while (cur.hasNext()){
+            Object id = cur.curr().get("_id");
+            DBCursor curalbum = album.find(new BasicDBObject("images", id));
+            if(!curalbum.hasNext()){
+                image.remove(new BasicDBObject("_id", id));
+            }
+            cur.next();
+        }
     }
 }
